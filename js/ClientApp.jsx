11c10b70fb21683b6c07565d0c4df28e2f050df8
Benjamin Myhre <ben.myhre@sundoginteractive.com>
2018-02-08 15:47:05 -0600

    get versioning of react and webpack all up and going.

---
+++ js/ClientApp.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const ce = React.createElement;
+
+const MyTitle = function(props) {
+	return ce('div', { style: { color: props.color } }, ce('h1', null, props.title));
+};
+
+const MyFirstComponent = function() {
+	return ce(
+		'div',
+		null,
+		ce(MyTitle, { color: 'rebeccapurple', title: 'House of Cards' }),
+		ce(MyTitle, { color: 'peru', title: 'Orange is the new black' }),
+		ce(MyTitle, { color: 'burlywood', title: 'Stranger Things' })
+	);
+};
+
+render(ce(MyFirstComponent), document.getElementById('app'));

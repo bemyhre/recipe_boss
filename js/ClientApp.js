310a2e0b9bee5753fade4a19287f63f8bd157eb7
Benjamin Myhre <ben.myhre@sundoginteractive.com>
2018-02-08 14:26:36 -0600

    added correct packages and fixe client app

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,11 +1,11 @@
 /* global React ReactDOM */
-var ce = React.createElement;
+const ce = React.createElement;
 
-var MyTitle = function(props) {
+const MyTitle = function(props) {
 	return ce('div', { style: { color: props.color } }, ce('h1', null, props.title));
 };
 
-var MyFirstComponent = function() {
+const MyFirstComponent = function() {
 	return ce(
 		'div',
 		null,

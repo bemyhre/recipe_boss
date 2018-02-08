c9aec7fdbf7515b7ca0dc64377729b7fc08b3094
Benjamin Myhre <ben.myhre@sundoginteractive.com>
2018-02-08 11:13:09 -0600

    added packege and start new day

---
+++ js/ClientApp.js
@@ -0,0 +1,18 @@
+/* global React ReactDOM */
+var ce = React.createElement;
+
+var MyTitle = function(props) {
+  return ce('div', { style: { color: props.color } }, ce('h1', null, props.title));
+};
+
+var MyFirstComponent = function() {
+  return ce(
+    'div',
+    null,
+    ce(MyTitle, { color: 'rebeccapurple', title: 'House of Cards' }),
+    ce(MyTitle, { color: 'peru', title: 'Orange is the new black' }),
+    ce(MyTitle, { color: 'burlywood', title: 'Stranger Things' })
+  );
+};
+
+ReactDOM.render(ce(MyFirstComponent), document.getElementById('app'));

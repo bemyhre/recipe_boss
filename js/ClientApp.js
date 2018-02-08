d4a1fa5352a0db86cffd74c7d8505f02a6b6e0d4
Benjamin Myhre <ben.myhre@sundoginteractive.com>
2018-02-08 13:03:39 -0600

    formatting stuff from prettier

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -2,17 +2,17 @@
 var ce = React.createElement;
 
 var MyTitle = function(props) {
-  return ce('div', { style: { color: props.color } }, ce('h1', null, props.title));
+	return ce('div', { style: { color: props.color } }, ce('h1', null, props.title));
 };
 
 var MyFirstComponent = function() {
-  return ce(
-    'div',
-    null,
-    ce(MyTitle, { color: 'rebeccapurple', title: 'House of Cards' }),
-    ce(MyTitle, { color: 'peru', title: 'Orange is the new black' }),
-    ce(MyTitle, { color: 'burlywood', title: 'Stranger Things' })
-  );
+	return ce(
+		'div',
+		null,
+		ce(MyTitle, { color: 'rebeccapurple', title: 'House of Cards' }),
+		ce(MyTitle, { color: 'peru', title: 'Orange is the new black' }),
+		ce(MyTitle, { color: 'burlywood', title: 'Stranger Things' })
+	);
 };
 
 ReactDOM.render(ce(MyFirstComponent), document.getElementById('app'));

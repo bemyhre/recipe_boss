2dc832c15d8c67979fb04628517da5f1e9ef8198
Benjamin Myhre <ben.myhre@sundoginteractive.com>
2018-02-12 08:11:49 -0600

    edited webpack config to match up to the docs

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@ const path = require('path');
 
 module.exports = {
 	context: __dirname,
-	entry: './js/ClientApp.jsx',
-	devtool: 'cheap-eval-source-map',
+	entry: './js/ClientApp.js',
+	devtool: 'source-map',
 	output: {
 		path: path.join(__dirname, 'public'),
 		filename: 'bundle.js'
@@ -11,23 +11,13 @@ module.exports = {
 	resolve: {
 		extensions: ['.js', '.jsx', '.json']
 	},
-	devServer: {
-		publicPath: '/public/',
-		historyApiFallback: true
-	},
 	stats: {
 		colors: true,
 		reasons: true,
-		chunks: true
+		chunks: false
 	},
 	module: {
 		rules: [
-			{
-				enforce: 'pre',
-				test: /\.jsx?$/,
-				loader: 'eslint-loader',
-				exclude: /node_modules/
-			},
 			{
 				test: /\.jsx?$/,
 				loader: 'babel-loader'

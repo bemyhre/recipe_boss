d3f977f169ea651a57d3747f87fc3b34de1e19fb
Benjamin Myhre <ben.myhre@sundoginteractive.com>
2018-02-13 15:12:14 -0600

    show data from json file

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import preload from '../data.json';
 
-const Search = () => <h1>bloop</h1>;
+const Search = () => (
+	<div className="search">
+		{preload.recipes.map(recipe => (
+			<div className="show-card">
+				<img alt={`${recipe.name} Recipe Poster`} src={`${recipe.imageUrl}`} />
+				<div>
+					<h3>
+						<a href={`${recipe.recipeUrl}`}>{recipe.name}</a>
+					</h3>
+					<div>
+						{recipe.ingredients.map(ingredient => (
+							<div className="ingredient-card">{ingredient.description}</div>
+						))}
+					</div>
+				</div>
+			</div>
+		))}
+	</div>
+);
 
 export default Search;

37319f25de35da8ca3bf3e0c0f5b35f4b8fe8b9b
Benjamin Myhre <ben.myhre@sundoginteractive.com>
2018-02-14 16:50:47 -0600

    just finished binding handlesearchterm

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import RecipeCard from './RecipeCard';
 import preload from '../data.json';
 
-const Search = () => (
-	<div className="search">
-		<header>
-			<h1>Recipe Boss</h1>
-			<input type="text" placeholder="Search" />
-		</header>
-		{preload.recipes.map(recipe => <RecipeCard key={recipe.recipeUrl} recipe={recipe} />)}
-	</div>
-);
+class Search extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			searchTerm: 'this is a debug statement'
+		};
+		this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
+	}
+
+	handleSearchTermChange(event) {
+		this.setState({ searchTerm: event.target.value });
+	}
+	render() {
+		return (
+			<div className="search">
+				<header>
+					<h1>Recipe Boss</h1>
+					<input
+						onChange={this.handleSearchTermChange}
+						value={this.state.searchTerm}
+						type="text"
+						placeholder="Search"
+					/>
+				</header>
+				{preload.recipes.map(recipe => <RecipeCard key={recipe.recipeUrl} recipe={recipe} />)}
+			</div>
+		);
+	}
+}
 
 export default Search;

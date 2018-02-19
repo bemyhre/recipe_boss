5cc6abcbd880a2272791f172bba2ef217c3023a0
Benjamin Myhre <ben.myhre35@gmail.com>
2018-02-19 16:50:54 -0600

    changed order of image

--- js/RecipeCard.jsx
+++ js/RecipeCard.jsx
@@ -8,11 +8,11 @@ const RecipeCard = props => (
       <h3>
         <a href={`${props.recipe.recipeUrl}`}>{props.recipe.name}</a>
       </h3>
+      <img alt={`${props.recipe.name} Recipe Poster`} src={`${props.recipe.imageUrl}`} />
       <div>
         {props.recipe.ingredients.map(ingredient => <IngredientCard key={ingredient.name} ingredient={ingredient} />)}
       </div>
     </div>
-    <img alt={`${props.recipe.name} Recipe Poster`} src={`${props.recipe.imageUrl}`} />
   </div>
 );
 
